08c1103191186dcdfae15a367a1038872c815771
Jia Jia <angersax@sina.com>
2022-07-02 08:33:45 +0800

    refactor(chore): update feature and test files

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+// copyright
+
+#include <iostream>
+
+int main() {
+    int i = 0;
+    int len = 10;
+
+    for (i = 0; i < len - 1; ++i) {
+        std::cout << i;
+    }
+
+    while (i < len -1) {
+        std::cout << i;
+    }
+
+    return 0;
+}
